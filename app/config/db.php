9e958a9e9720a974e156f1784dc5d8da916b22fb
tona-lc <nvtoan@lockon-vn.com>
2018-01-25 11:50:37 +0700

    Setup db.

--- app/config/db.php
+++ app/config/db.php
@@ -2,13 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mysql_test',
+    'dsn' => 'mysql:host=localhost;dbname=production_db',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+//    Schema cache options (for production environment)
+//    'enableSchemaCache' => true,
+//    'schemaCacheDuration' => 60,
+//    'schemaCache' => 'cache',
 ];
