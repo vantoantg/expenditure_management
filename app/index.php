3c03210b1a7200313719336dff19b431297922dc
tona-lc <nvtoan@lockon-vn.com>
2018-02-09 17:47:17 +0700

    test.

--- app/index.php
+++ app/index.php
@@ -1,5 +1,8 @@
 <?php
-
+echo '<pre>';
+print_r(121212);
+echo '</pre>';
+die;
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
